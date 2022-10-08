f16e7d2a3585eb3c68a9076e3eba37c82c9106b3
INotFime <83116565+INotFime@users.noreply.github.com>
2022-10-08 21:29:29 +0300

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,9 @@
+import os
+
+def install(package):
+    print(f"Installing {package}")
+    os.system(f"pip install {package}")
+
+install("dearpygui")
+install("portablemc")
+input("Finished installing PingasLauncher. Now you can launch it through main.py file.\nPress Enter To Exit")
